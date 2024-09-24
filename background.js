4aaa6ca8b18e8eac1ec1bf894f4be7d17c9053ed
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-09-24 13:02:16 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
+let extractedResults = [];
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === 'storeResults') {
+    extractedResults = message.results;
+    console.log('Extracted Results:', extractedResults);
+  }
+});
 
